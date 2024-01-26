560749065dde596961128b13cf4a5895f6fcb8d5
HafeezUllah886 <Hafeezullah886@gmail.com>
2024-01-26 14:12:37 +0500

    migration changed

--- database/migrations/2024_01_26_061213_create_database_backups_table.php
+++ database/migrations/2024_01_26_061213_create_database_backups_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('database_backups', function (Blueprint $table) {
             $table->id();
-            $table->string('size');
             $table->text('path');
             $table->timestamps();
         });
