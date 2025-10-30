69b95447ce35d6a18630c94234a2d908213fcb1a
HafeezUllah886 <Hafeezullah886@gmail.com>
2025-10-30 12:45:37 +0500

    done

--- resources/views/stock/transfer/index.blade.php
+++ resources/views/stock/transfer/index.blade.php
@@ -22,6 +22,7 @@
                     <th>Warehouse</th>
                     <th>Type</th>
                     <th>Expense</th>
+                    <th>Notes</th>
                     <th>Status</th>
                     <th>Actions</th>
                 </tr>
@@ -48,6 +49,7 @@
                         <td>{{ $transfer->from == auth()->user()->warehouseID ? $transfer->to_warehouse->name : $transfer->from_warehouse->name }}</td>
                         <td>{{ $transfer->from == auth()->user()->warehouseID ? "Outgoing" : "Incoming" }}</td>
                         <td>{{ $transfer->expense }}</td>
+                        <td>{{ $transfer->notes }}</td>
                         <td><span class="badge {{$badgeColor}}">{{ $transfer->status }}</span></td>
                         <td>
                             <div class="dropdown">
